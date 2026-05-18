b52917d5a83703169310e7b63ee348ca3231d01a
Yeison23Rodriguez09 <maffiarodriguez09@gmail.com>
2026-05-18 18:27:53 -0500

    Fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
+set -e;
 
-# Salir de inmediato si un comando falla
-set -e
+echo "==> Ejecutando Migraciones...";
+python manage.py migrate --noinput;
 
-echo "==> Ejecutando Migraciones..."
-python manage.py migrate --noinput
+echo "==> Recopilando estáticos...";
+python manage.py collectstatic --noinput;
 
-echo "==> Recopilando estáticos..."
-python manage.py collectstatic --noinput
-
-echo "==> Iniciando Gunicorn..."
-exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
-
-python manage.py migrate --noinput
+echo "==> Iniciando Gunicorn...";
+exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT;
